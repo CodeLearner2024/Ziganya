20b54ac4e7cd8d76b913fc9ecec81b2c02f9cee1
NDIHOKUBWAYO Eric <endihokubwayo41@gmail.com>
2025-10-07 09:27:15 +0200

    api contribution

---
+++ src/main/java/com/codeLearner/Ziganya/models/contribution/ContributionController.java
@@ -0,0 +1,44 @@
+package com.codeLearner.Ziganya.models.contribution;
+
+import com.codeLearner.Ziganya.util.DeleteOperationResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contributions")
+public class ContributionController {
+
+    private final ContributionService contributionService;
+
+    public ContributionController(ContributionService contributionService) {
+        this.contributionService = contributionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ContributionResponse> createContribution(@RequestBody ContributionRequest request) {
+        ContributionResponse contributionResponse = contributionService.createContribution(request);
+        return new ResponseEntity<>(contributionResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ContributionResponse>> getAllContributions() {
+        List<ContributionResponse> contributionResponse = contributionService.getAllContributions();
+        return new ResponseEntity<>(contributionResponse, HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ContributionResponse> updateContribution(@PathVariable Long id, @RequestBody ContributionRequest request) {
+        ContributionResponse contributionResponse = contributionService.updateContribution(id, request);
+        return new ResponseEntity<>(contributionResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<DeleteOperationResponse> deleteContribution(@PathVariable Long id) {
+        DeleteOperationResponse response = contributionService.deleteContribution(id);
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+}
